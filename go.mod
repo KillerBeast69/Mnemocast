4693a89e733de565ef219f7501bc06f0beeffdd8
KillerBeast69 <omshinde2803@gmail.com>
2026-08-09 18:42:09 +0530

    fetch youtube video

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KillerBeast69/Mnemocast
+
+go 1.26.5
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// defining structs to tell Go exactly how to map the XML data into variables
+
+type Feed struct {
+	XMLName xml.Name `xml:"feed"`
+	Title   string   `xml:"title"`
+	Entries []Entry  `xml:"entry"`
+}
+
+type Entry struct {
+	Title   string `xml:"title"`
+	VideoID string `xml:"videoId"` // youtube's custom tag for video ID
+	Link    Link   `xml:"link"`
+}
+
+type Link struct {
+	Href string `xml:"href,attr"` // the "attr" option tells Go to grab the attribute inside the tag, not the text between it
+}
+
+func main() {
+	// Gordan Ramsay's YouTube channel ID
+	channelID := "UCIEv3lZ_tNXHzL3ox-_uUGQ"
+	url := "https://www.youtube.com/feeds/videos.xml?channel_id=" + channelID
+
+	fmt.Printf("Fetching the latest video for channel %s...\n", channelID)
+
+	// make the HTTP GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err) //  if there is no internat or the URL is invalid, crash and print the error
+	}
+
+	defer resp.Body.Close() // Ensure we close the connection when the function exits
+
+	// read the raw XML data from the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err) // if we can't read the response body, crash and print the error
+	}
+
+	// unmarshal(parse) the raw XML data into our Go structs
+	var feed Feed
+	if err := xml.Unmarshal(body, &feed); err != nil {
+		panic(err) // if we can't parse the XML, crash and print the error
+	}
+
+	// print the results
+	fmt.Printf("Feed Title: %s\n", feed.Title)
+	if len(feed.Entries) > 0 {
+		latestVideo := feed.Entries[0]
+		fmt.Println("--------------------------------------------------")
+		fmt.Printf("Channel: %s\n", feed.Title)
+		fmt.Printf("Latest Video Title: %s\n", latestVideo.Title)
+		fmt.Printf("Video ID: %s\n", latestVideo.VideoID)
+		fmt.Printf("Link: %s\n", latestVideo.Link.Href)
+	} else {
+		fmt.Println("No videos found :/.")
+	}
+}
