5212088408641bc3943ca01c4d38e31f2c4c7ea6
om <omshinde2803@gmail.com>
2026-08-18 04:59:30 +0000

    goose migrations

---
+++ store/migrations/001_inti.sql
@@ -0,0 +1,21 @@
+-- FILE: store/migrations/001_init.sql
+
+-- +goose Up
+CREATE TABLE channels (
+    channel_id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    added_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE videos (
+    video_id TEXT PRIMARY KEY,
+    channel_id TEXT NOT NULL REFERENCES channels(channel_id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    url TEXT NOT NULL,
+    is_summarized BOOLEAN NOT NULL DEFAULT FALSE,
+    published_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+-- +goose Down
+DROP TABLE videos;
+DROP TABLE channels;
